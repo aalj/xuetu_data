c08b5160b6e1a298fdd430d2b6d06f86d19058c1
liangjun <dqliangjun@126.com>
2016-01-29 21:03:09 +0800

    20160129提交,

---
+++ xuetuWeb/xuetuWeb/src/com/xuetu/entity/Student.java
@@ -0,0 +1,37 @@
+/**
+ * Student.java
+ * com.xuetu.entity
+ *
+ * Function： TODO 
+ *
+ *   ver     date      		author
+ * ──────────────────────────────────
+ *   		 2016年1月29日 		Stone
+ *
+ * Copyright (c) 2016, TNT All Rights Reserved.
+*/
+
+package com.xuetu.entity;
+/**
+ * ClassName:Student
+ * Function: 学生实体
+ * Reason:	 TODO ADD REASON
+ *
+ * @author   Stone
+ * @version  
+ * @since    Ver 1.1
+ * @Date	 2016年1月29日		上午11:15:10
+ *
+ * @see 	 
+
+ */
+public class Student {
+	
+	
+	
+	
+	
+	
+
+}
+
