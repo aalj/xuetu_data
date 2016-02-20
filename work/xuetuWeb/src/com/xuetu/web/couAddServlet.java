696878fc24fe9fd7ae2c39429e83386becbc030d
woshikangyi <taoyannia1>
2016-02-20 14:05:18 +0800

    宿舍

---
+++ work/xuetuWeb/src/com/xuetu/web/couAddServlet.java
@@ -0,0 +1,37 @@
+package com.xuetu.web;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class couAddServlet
+ */
+@WebServlet("/couAddServlet")
+public class couAddServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("utf-8");
+		response.setCharacterEncoding("utf-8");
+		/*将页面数据获取到，封装成一个Coupon类，调用Service层的方法添加到数据库
+		 * */
+		request.getParameter("cou_redeem_points");
+		request	}
+
+}
